b18100374da8181eab7b5978b36926b40c12fbc7
honorezemagho <honorezemagho@gmail.com>
2022-08-08 11:49:56 +0100

    removed unused js, added data-product to hold product details and started with event listeners

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let prohtml = "";
 let produits = fetch("produits.json")
   .then((res) => res.json())
   .then((data) => {
-    // console.log(JSON.stringify(data));
     const customDiv = document.getElementById("products");
     localStorage.setItem("all-products", JSON.stringify(data));
     for (const item of data) {
@@ -13,19 +12,6 @@ let produits = fetch("produits.json")
     }
     customDiv.innerHTML = prohtml;
   });
-//  const json = {
-//         "id": 11,
-//         "title": "perfume Oil",
-//         "description": "Mega Discount, Impression of Acqua Di Gio by GiorgioArmani concentrated attar perfume Oil",
-//         "price": 13,
-//         "discountPercentage": 8.4,
-//         "rating": 4.26,
-//         "stock": 65,
-//         "brand": "Impression of Acqua Di Gio",
-//         "category": "fragrances",
-//         "thumbnail": "https://dummyjson.com/image/i/products/12/thumbnail.jpg",
-//        }
-//        console.log (json)
 
 function productElement(product) {
   return `
@@ -42,27 +28,30 @@ function productElement(product) {
               </div>
               <h4>${product.price} FCFA </h4>
                 </div>
-               <button data onclick="${new Panier().addProduct(
-                 product
-               )}"> <i class="fa-solid fa-cart-shopping cart "> </i> </button>
+               <button data-product=${product}> <i class="fa-solid fa-cart-shopping cart "> </i> </button>
              </div>
            `;
 }
 
+// 
+window.addEventListener('DOMContentLoaded', function () {
+  const siteProducts = document.getElementsByClassName("pro");
+})
+
 class Panier {
   constructor() {
     let panier = localStorage.getItem("panier");
-    let allProduits = localStorage.getItem("all-products");
+    let allProducts = localStorage.getItem("all-products");
     if (panier == null) {
       this.panier = [];
     } else {
       this.panier = JSON.parse(panier);
     }
 
-    if (allProduits == null) {
-      this.allProduits = [];
+    if (allProducts == null) {
+      this.allProducts = [];
     } else {
-      this.allProduits = JSON.parse(allProduits);
+      this.allProducts = JSON.parse(allProducts);
     }
   }
 
@@ -75,7 +64,7 @@ class Panier {
     if (foundproduit != undefined) {
       this.changeQuantity(foundproduit, 1);
     } else {
-      let productAvailable = this.allProduits.find((p) => p.id == product.id);
+      let productAvailable = this.allProducts.find((p) => p.id == product.id);
 
       if (productAvailable != undefined) {
         productAvailable.quantity = 1;
@@ -118,79 +107,3 @@ class Panier {
     return total;
   }
 }
-
-//        function getproduct(){
-//         let product = localStorage.getItem("product");
-//         if (product == null){
-//          return[];
-//         }else{
-//           return JSON.parse(product);
-//         }
-//        }
-// function addproduct (produits){
-//         let product = getproduct();
-//         let foundproduits = basket.find(p => p.id == product.id);
-//         if(foundproduits != undefined){
-//           foundproduits.quantity++;
-//         }else{
-//           produits.quantity = 1;
-//           product.push(produits);
-//         }
-//        saveproduct(product);
-//        }
-// function removeFromproduct(produits){
-//         let product = getproduct();
-//         product = product.filter(p => p.id != produits.id);
-//         saveproduct(product);
-//        }
-
-// function changeQuantity(produits,quantity){
-//         let product = getproduct();
-//         let foundproduits = basket.find(p => p.id == product.id);
-//         if(foundproduits != undefined){
-//           foundproduits.quantity += quantity;
-//            if (foundProduct.quantity <= 0){
-//             removeFromproduct(product);
-//            }else{
-//             saveBasket(basket);
-//            }
-//        }
-
-//       }
-
-// function getNumberProduct(){
-//   let basket = getBasket();
-//   let number = 0;
-//   for(let product of basket){
-//     number += produit.quantity;
-//   }
-//   return number;
-// }
-
-// function getTotalPrice(){
-//   let basket = getBasket();
-//   let total = 0;
-//   for(let product of basket){
-//     number += produit.quantity * product.price;
-//   }
-//   return total;
-// }
-
-//   customDiv.innerHTML = product;
-
-//    const json = {
-//     "id": 11,
-//     "title": "perfume Oil",
-//     "description": "Mega Discount, Impression of Acqua Di Gio by GiorgioArmani concentrated attar perfume Oil",
-//     "price": 13,
-//     "discountPercentage": 8.4,
-//     "rating": 4.26,
-//     "stock": 65,
-//     "brand": "Impression of Acqua Di Gio",
-//     "category": "fragrances",
-//     "thumbnail": "https://dummyjson.com/image/i/products/11/thumbnail.jpg",
-//    }
-//    const stringJson = JSON.stringify(json);
-//    const parsedString = JSON.parse(stringJson)
-
-//    console.log (json)
